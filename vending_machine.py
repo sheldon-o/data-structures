9f0e0cc81b4d631405f2366dffded617f0067f08
shivam arora <85953131+sheldon-o@users.noreply.github.com>
2022-03-11 21:48:41 +0530

    Create vending_machine.py

---
+++ vending_machine.py
@@ -0,0 +1,14 @@
+class VendingMachine():
+  def __init(self,num_items,item_price):
+    self.num_items=num_items
+    self.item_price=item_price
+  def buy(self,req_items,money):
+    if (req_items <= self.num_items):
+      x = req_items*self.item_price
+      if( money>=x):
+        self.num_items=self.num_items-req_items
+        return (money-x)
+      else:
+        return ("not enough coins")
+   else:
+    return ("not enough items in the machine")
